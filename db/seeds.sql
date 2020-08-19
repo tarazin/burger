3b0a1a22c93448293e3d0ad955a52feea7e0216b
Zahra Azin <ztara.azin@gmail.com>
2020-08-19 06:53:23 -0400

    update seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
-INSERT INTO burger (burger_name, devoured) VALUES ("Cheese Burger", false);   
-INSERT INTO burger (burger_name, devoured) VALUES ("Bacon Burger", false);   
-INSERT INTO burger (burger_name, devoured) VALUES ("Chicken Burger", false);   
-INSERT INTO burger (burger_name) VALUES ("Vegan Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", false);   
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", false);   
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Burger", false);   
+INSERT INTO burgers (burger_name, devoured) VALUES ("Vegan Burger", false);
 
-SELECT * FROM burger;
-UPDATE burger SET devoured = 1 WHERE id = 1;
+SELECT * FROM burgers;
+UPDATE burgers SET devoured = 1 WHERE id = 1;
